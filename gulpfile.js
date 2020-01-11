705e42c3ca9dc37b0ff3abb642fb9f3be113af0c
dotvhs <voythas@gmail.com>
2020-01-11 07:20:56 +0100

    Work in progress

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require("gulp"),
 	cleanCSS = require("gulp-clean-css");
 rename = require("gulp-rename");
 
-var fileName = "template.user.css";
+var fileName = "boardgamegeek-dark.user.css";
 
 var paths = {
 	dest: ".",
